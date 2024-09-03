f434a6a9e0f1ea560d5edfc9c0f42f167e98d763
Nilton taco <nilton2005marzo@gmail.com>
2024-09-03 17:20:43 -0500

    Subiendo el proyecto vacío

---
+++ app/src/main/java/com/example/apk_administration/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.apk_administration
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.apk_administration.ui.theme.Apk_administrationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Apk_administrationTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Apk_administrationTheme {
+    }
+}
